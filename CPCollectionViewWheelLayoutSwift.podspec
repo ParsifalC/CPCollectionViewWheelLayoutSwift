7881a7cc4d0f46b8decf13b22ac9f4cc36016099
Parsifal <zmw@izmw.me>
2017-01-09 21:14:25 +0800

    refactoring for Cocoapods

--- CPCollectionViewWheelLayoutSwift.podspec
+++ CPCollectionViewWheelLayoutSwift.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'CPCollectionViewWheelLayoutSwift'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of CPCollectionViewWheelLayoutSwift.'
+  s.summary          = 'An awesome layout for UICollcetionView'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,14 +18,14 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+  An awesome layout for UICollcetionView
                        DESC
 
-  s.homepage         = 'https://github.com/<GITHUB_USERNAME>/CPCollectionViewWheelLayoutSwift'
+  s.homepage         = 'https://github.com/parsifalc/CPCollectionViewWheelLayout-Swift'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'Parsifal' => 'zmw@izmw.me' }
-  s.source           = { :git => 'https://github.com/<GITHUB_USERNAME>/CPCollectionViewWheelLayoutSwift.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/parsifalc/CPCollectionViewWheelLayout-Swift.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '8.0'
---
+++ Example/CPCollectionViewWheelLayoutSwift/CPViewController.swift
@@ -0,0 +1,76 @@
+//
+//  CPViewController.swift
+//  CPCollectionViewWheelLayout-Swift
+//
+//  Created by Parsifal on 2016/12/29.
+//  Copyright © 2016年 Parsifal. All rights reserved.
+//
+
+import UIKit
+import CPCollectionViewWheelLayoutSwift
+
+class CPViewController: UIViewController, UICollectionViewDataSource {
+    
+    fileprivate let reuseIdentifier = "CPCollectionViewCell"
+    var colletionView:UICollectionView?
+    open var wheelType = CPWheelLayoutType.leftBottom
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup views
+        let layout = UICollectionViewFlowLayout.init()
+        layout.itemSize = CGSize.init(width: 100, height: 100)
+        layout.scrollDirection = .vertical
+        let configuration = CPWheelLayoutConfiguration.init(withCellSize: CGSize.init(width: 100, height: 100), radius: 200, angular: 20, wheelType:wheelType)
+        let wheelLayout = CPCollectionViewWheelLayout.init(withConfiguration: configuration)
+        colletionView = UICollectionView.init(frame: view.frame, collectionViewLayout:wheelLayout)
+        colletionView?.showsVerticalScrollIndicator = false
+        colletionView?.showsHorizontalScrollIndicator = false
+        colletionView?.backgroundColor = .white
+        colletionView?.register(CPCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        colletionView?.dataSource = self
+        view.addSubview(colletionView!)
+        view.sendSubview(toBack: colletionView!)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CPCollectionViewCell
+        cell.textLabel.text = String(indexPath.row)
+        return cell
+    }
+    
+    @IBAction func cellSizeChanged(_ sender: UISlider) {
+        var configuration = (colletionView?.collectionViewLayout as! CPCollectionViewWheelLayout).configuration
+        configuration.cellSize = CGSize.init(width: Double(sender.value), height: Double(sender.value))
+        updateCollectionView(withLayoutConfiguration: configuration)
+    }
+
+    @IBAction func angularViewChanged(_ sender: UISlider) {
+        var configuration = (colletionView?.collectionViewLayout as! CPCollectionViewWheelLayout).configuration
+        configuration.angular = Double(sender.value)
+        updateCollectionView(withLayoutConfiguration: configuration)
+    }
+    
+    @IBAction func radiusValueChanged(_ sender: UISlider) {
+        var configuration = (colletionView?.collectionViewLayout as! CPCollectionViewWheelLayout).configuration
+        configuration.radius = Double(sender.value)
+        updateCollectionView(withLayoutConfiguration: configuration)
+    }
+    
+    func updateCollectionView(withLayoutConfiguration configuration:CPWheelLayoutConfiguration) {
+        let newLayout = CPCollectionViewWheelLayout.init(withConfiguration: configuration)
+        colletionView?.collectionViewLayout.invalidateLayout()
+        colletionView?.collectionViewLayout = newLayout
+        colletionView?.reloadData()
+    }
+    
+    @IBAction func dismissButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true) {
+        }
+    }
+}
